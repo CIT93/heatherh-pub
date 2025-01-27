e3ba637be28926c8f9609053d9c21dbdd7333573
Heather Hoffman <chuckkitty@gmail.com>
2025-01-27 12:42:03 -0800

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,22 @@
-console.log("Week 1 -Hello Word");
+console.log("Hello from inside the main.js file");
+
+// let myVar;
+
+const myVar = "heather"
+
+const myVarType = typeof(myVar);
+
+console.log("My Var Type: " ,myVarType);
+console.log(`My Var Type: ${myVarType}`);
+
+
+
+function runNow (){
+  if (myVarType === "number") {
+    console.log(`will 11 this one run?`);
+  } else {
+    console.log(`will 13 this one run?`);
+  }
+}
+runNow();
+runNow();

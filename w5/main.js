747245fdb985313a2e63db975aba5a765ea16bed
Heather Hoffman <chuckkitty@gmail.com>
2025-02-12 16:03:21 -0800

    output complete

--- w5/main.js
+++ w5/main.js
@@ -48,43 +48,21 @@ function start(houseHoldMembers, houseSize) {
     cfptotal: total,
   });
 
-  function displayOutObj(obj) {
-    const output = document.getElementById("output");
-    const newP = document.createElement("p");
-    const newH2 = document.createElement("h2");
-    newH2.textContent = `Carbon Footprint total is ${obj.cfptotal}`;
-    newP.textContent = `This number is based on ${obj.houseM} people (score: ${obj.houseMPTS}) in a house size of ${obj.houseS} (score: ${obj.houseSPTS})`;
-    output.appendChild(newH2);
-    output.appendChild(newP);
-  }
 }
-// function start(houseHoldMembers, houseSize) {
-//   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
-//   const houseSizePts = determineHouseSizePts(houseSize);
-//   const total = houseHoldPTS + houseSizePts;
-//   cfpData.push([
-//     houseHoldMembers,
-//     houseSize,
-//     houseHoldPTS,
-//     houseSizePts,
-//     total,
-//   ]);
-// }
 
 function displayOutput() {
   for (obj of cfpData) {
     console.log(obj);
     const output = document.getElementById("output");
-    //         const newP = document.createElement("p");
+    const newP = document.createElement("p");
     const newH2 = document.createElement("h2");
     newH2.textContent = `Carbon Footprint total is ${obj.cfptotal}`;
-    //         const newH3 = document.createElement("h3");
-    //         newH3.textContent = `Based on number in household and size of home`;
-    //         newP.textContent = `This number is based on the number of people in the household of (score: ${arr[0]})`;
-    //         newP.textContent += ` and a (score: ${arr[1]}) size of home`;
-    //         output.appendChild(newH3);
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `Based on number in household and size of home`;
+    newP.textContent = `This number is based on ${obj.houseM} people (score: ${obj.houseMPTS}) in a house size of ${obj.houseS} (score: ${obj.houseSPTS})`;
     output.appendChild(newH2);
-    //         output.appendChild(newP);
+    output.appendChild(newH3);
+    output.appendChild(newP);
   }
 }
 
@@ -94,6 +72,7 @@ start(3, "medium");
 start(4, "large");
 console.log(cfpData);
 displayOutput();
+
 //refactor displayOutput() with a for loop.
 
 // function displayOutput() {

3a853ffc009aae8dacf3f4999fce65284c831ecf
Heather Hoffman <chuckkitty@gmail.com>
2025-03-01 14:47:33 -0800

    cfp commented out

--- w5/main.js
+++ w5/main.js
@@ -35,79 +35,79 @@ function displayPets() {
 
 displayPets();
 
-const cfpData = [];
+// const cfpData = [];
 
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large") {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 7;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apt") {
-    houseSizePoints = 2;
-  }
-  return houseSizePoints;
-}
+// function determineHouseSizePts(size) {
+//   let houseSizePoints = 0;
+//   if (size === "large") {
+//     houseSizePoints = 10;
+//   } else if (size === "medium") {
+//     houseSizePoints = 7;
+//   } else if (size === "small") {
+//     houseSizePoints = 4;
+//   } else if (size === "apt") {
+//     houseSizePoints = 2;
+//   }
+//   return houseSizePoints;
+// }
 
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPoints = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPoints = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPoints = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPoints = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPoints = 2;
-  }
-  return houseHoldPoints;
-}
-// refactor the start function to store the data in an object
+// function determineHouseHoldPts(numberInHousehold) {
+//   let houseHoldPoints = 0;
+//   if (numberInHousehold === 1) {
+//     houseHoldPoints = 14;
+//   } else if (numberInHousehold === 2) {
+//     houseHoldPoints = 12;
+//   } else if (numberInHousehold === 3) {
+//     houseHoldPoints = 10;
+//   } else if (numberInHousehold === 4) {
+//     houseHoldPoints = 8;
+//   } else if (numberInHousehold === 5) {
+//     houseHoldPoints = 6;
+//   } else if (numberInHousehold === 6) {
+//     houseHoldPoints = 4;
+//   } else if (numberInHousehold > 6) {
+//     houseHoldPoints = 2;
+//   }
+//   return houseHoldPoints;
+// }
+// // refactor the start function to store the data in an object
 
-function start(houseHoldMembers, houseSize) {
-  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
-  const houseSizePts = determineHouseSizePts(houseSize);
-  const total = houseHoldPTS + houseSizePts;
+// function start(houseHoldMembers, houseSize) {
+//   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+//   const houseSizePts = determineHouseSizePts(houseSize);
+//   const total = houseHoldPTS + houseSizePts;
 
-  cfpData.push({
-    houseM: houseHoldMembers,
-    houseS: houseSize,
-    houseMPTS: houseHoldPTS,
-    houseSPTS: houseSizePts,
-    cfptotal: total,
-  });
+//   cfpData.push({
+//     houseM: houseHoldMembers,
+//     houseS: houseSize,
+//     houseMPTS: houseHoldPTS,
+//     houseSPTS: houseSizePts,
+//     cfptotal: total,
+//   });
 
-}
+// }
 
-function displayOutput() {
-  const output = document.getElementById("output");
-  for (obj of cfpData) {
-    const newP = document.createElement("p");
-    const newH2 = document.createElement("h2");
-    newH2.textContent = `Carbon Footprint total is ${obj.cfptotal}`;
-    const newH3 = document.createElement("h3");
-    newH3.textContent = `Based on number in household and size of home`;
-    newP.textContent = `This number is based on ${obj.houseM} people (score: ${obj.houseMPTS}) in a house size of ${obj.houseS} (score: ${obj.houseSPTS})`;
-    output.appendChild(newH2);
-    output.appendChild(newH3);
-    output.appendChild(newP);
-  }
-}
+// function displayOutput() {
+//   const output = document.getElementById("output");
+//   for (obj of cfpData) {
+//     const newP = document.createElement("p");
+//     const newH2 = document.createElement("h2");
+//     newH2.textContent = `Carbon Footprint total is ${obj.cfptotal}`;
+//     const newH3 = document.createElement("h3");
+//     newH3.textContent = `Based on number in household and size of home`;
+//     newP.textContent = `This number is based on ${obj.houseM} people (score: ${obj.houseMPTS}) in a house size of ${obj.houseS} (score: ${obj.houseSPTS})`;
+//     output.appendChild(newH2);
+//     output.appendChild(newH3);
+//     output.appendChild(newP);
+//   }
+// }
 
-start(1, "apt");
-start(2, "small");
-start(3, "medium");
-start(4, "large");
-console.log(cfpData);
-displayOutput();
+// start(1, "apt");
+// start(2, "small");
+// start(3, "medium");
+// start(4, "large");
+// console.log(cfpData);
+// displayOutput();
 
 //refactor displayOutput() with a for loop.
 

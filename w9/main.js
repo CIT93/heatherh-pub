e636265dc34690279c3f0d8a6dcf387c72cf8b6e
Heather Hoffman <chuckkitty@gmail.com>
2025-04-16 14:07:05 -0700

    code after implementing solution

--- w9/main.js
+++ w9/main.js
@@ -3,20 +3,15 @@ import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
 import { FORM } from "./global.js";
 import { cfpData, saveLS, getLS } from "./storage.js";
 
+const firstnameEL = document.getElementById("firstname");
+const lastnameEL = document.getElementById("lastname");
+const submitEL = document.getElementById("submitError");
 
-// const FORM = document.getElementById("form");
-// const output = document.getElementById("output");
-// const cfpData = [];
-
-
-
-function start(houseHoldMembers, houseSize) {
+function start(firstName, lastName, houseHoldMembers, houseSize) {
 
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePts = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePts;
-  const firstName = FORM.firstname.value;
-  const lastName = FORM.lastname.value;
   cfpData.push({
     firName: firstName,
     lasName: lastName,
@@ -29,23 +24,6 @@ function start(houseHoldMembers, houseSize) {
 }
 
 renderTbl(cfpData);
-
-FORM.addEventListener("submit", function(e){
-  e.preventDefault();
-  const firstNameIsValid = document.getElementById('firstname').value !== '';
-  const lastNameIsValid = document.getElementById('lastname').value !== '';
-  if (firstNameIsValid && lastNameIsValid) {
-    alert('Form is valid. You can proceed with submitting the form to the server.');
-    }
-  const houseHoldM = parseInt(FORM.housem.value);
-  const houseS = FORM.houses.value;
-
-  start(houseHoldM, houseS);
-  // output.innerHTML = "";
-  saveLS(cfpData);
-  renderTbl(cfpData)
-  FORM.reset();
-});
 
 const validateField = event => {
   const field = event.target.value;
@@ -61,8 +39,38 @@ const validateField = event => {
   }
   };
 
-document.getElementById('firstname').addEventListener('blur', validateField);
-document.getElementById('lastname').addEventListener('blur', validateField);
+firstnameEL.addEventListener('blur', validateField);
+lastnameEL.addEventListener('blur', validateField);
+
+FORM.addEventListener("submit", function(e){
+  e.preventDefault();
+  submitEL.textContent = "";
+  const firstName = FORM.firstname.value;
+  const lastName = FORM.lastname.value;
+  const firstNameIsValid = firstnameEL.value !== '';
+  const lastNameIsValid = lastnameEL.value !== '';
+  if (firstNameIsValid && lastNameIsValid) {
+    const houseHoldM = parseInt(FORM.housem.value);
+    const houseS = FORM.houses.value;
+    start(firstName, lastName, houseHoldM, houseS);
+    saveLS(cfpData);
+    renderTbl(cfpData)
+    FORM.reset();
+  }
+   else {
+    submitEL.textContent = "Form requires first name and last name";
+  }
+  // const houseHoldM = parseInt(FORM.housem.value);
+  // const houseS = FORM.houses.value;
+
+  // start(houseHoldM, houseS);
+  // // output.innerHTML = "";
+  // saveLS(cfpData);
+  // renderTbl(cfpData)
+  // FORM.reset();
+});
+
+
 
 // window.addEventListener("load", (e) => {
 //   renderTbl(cfpData);

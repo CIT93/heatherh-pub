03a4c7450c6880936d04531ca6e98c8b2954d1f1
Heather Hoffman <chuckkitty@gmail.com>
2025-04-26 11:47:45 -0700

    my code before watching solution

---
+++ w11/main.js
@@ -0,0 +1,46 @@
+import { renderTbl } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+import { FORM, FNAME, LNAME, SUBMIT } from "./global.js";
+import { cfpData, saveLS, getLS } from "./storage.js";
+import { FP } from "./fp.js";
+
+renderTbl(cfpData);
+
+const validateField = (event) => {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === "") {
+    fieldError.textContent = `${fieldId} is required`;
+    event.target.classList.add("invalid");
+  } else {
+    fieldError.textContent = "";
+    event.target.classList.remove("invalid");
+  }
+};
+
+FNAME.addEventListener("blur", validateField);
+LNAME.addEventListener("blur", validateField);
+
+FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  SUBMIT.textContent = "";
+
+  if (FNAME.value !== "" && LNAME.value !== "") {
+    const fpObj = new FP(
+      FNAME.value,
+      LNAME.value,
+      parseInt(FORM.housem.value),
+      FORM.houses.value,
+      FORM.foodC.value
+    );
+    cfpData.push(fpObj);
+    console.log(cfpData);
+    saveLS(cfpData);
+    renderTbl(cfpData);
+    FORM.reset();
+  } else {
+    SUBMIT.textContent = "Form requires first name and last name";
+  }
+});

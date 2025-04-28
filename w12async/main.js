61d79eeaa4564717a35ac7689648baa080938ed6
Heather Hoffman <chuckkitty@gmail.com>
2025-04-28 10:48:58 -0700

    w12 my attempt at the code, before I review other students code

---
+++ w12async/main.js
@@ -0,0 +1,23 @@
+const FORM = document.getElementById("form");
+const DIV1 = document.getElementById("workoutOutput");
+const DIV2 = document.getElementById("asyncOutput");
+
+FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  DIV1.textContent = `Start ${FORM.workout.value} < > Goal reps is ${FORM.reps.value}`;
+
+  function repTime(callback) {
+    setTimeout(() => {
+      const repStop = FORM.workout.value;
+      callback(repStop);
+    }, FORM.seconds.value * 1000);
+  }
+
+  function stopWorkout(repStop) {
+    DIV2.textContent = `Stop ${repStop}`;
+  }
+
+  repTime(stopWorkout);
+  // console.log(`${FORM.seconds.value}`);
+});
+
